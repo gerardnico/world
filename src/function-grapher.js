eb853eacaa87316042833c7a5d9bd86ed0be3a74
Gerardnico <gerardnico@gerardnico.com>
2017-11-27 16:56:23 +0100

    The grapher is now a class and Xmin, Xmax and step can now be passed as setter

--- src/function-grapher.js
+++ src/function-grapher.js
@@ -1,24 +1,76 @@
+class Grapher {
 
-// Define a Person as an Object Literal
-var grapher = function(func){
-    this.func = func;
-    this.getData = function getData(){
+    constructor(func) {
+        this.func = func;
+        this.xMax = 10;
+        this.xMin = -10;
+        this.step = 0.1;
+    }
+
+    getData() {
+
+        // Checks
+        if (this.xMin > this.xMax) {
+            throw "The xMin value (" + this.xMin + ") cannot be greater than the actual xMax value (" + this.xMax + ")";
+        }
+
         var data = [];
-        for (var i=this.xMin;i<this.xMax;i=i+this.step){
-            data.push({ "x" : i, "y": Math.cos(i)});
+        for (var i = this.xMin; i < this.xMax; i = i + this.step) {
+            data.push({"x": i, "y": this.func(i)});
         }
         return data;
     }
-};
 
-// Set the prototype to be the person object (dynamic inheritance)
-// All new ColoredPerson must inherit its properties.
-grapher.prototype = {
-    xMax : 10,
-    xMin : -10,
-    step : 0.1
-};
+    setStep(step) {
+
+        this.step = Math.abs(step);
+
+    }
+
+    /**
+     * Set the X minimal value of the graph
+     * Value checks are done when running any function (not at the setter level)
+     * @param number
+     */
+    setXMin(number) {
+
+        this.xMin = number;
 
 
-export {grapher as default};
+    }
+
+    /**
+     * Set the X maximal value of the graph
+     * Value checks are done when running any function (not at the setter level)
+     * @param number
+     */
+    setXMax(number) {
+
+        this.xMax = number;
+
+    }
+}
+
+// Define a Person as an Object Literal
+// var grapher = function(func){
+//     this.func = func;
+//     this.getData = function getData(){
+//         var data = [];
+//         for (var i=this.xMin;i<this.xMax;i=i+this.step){
+//             data.push({ "x" : i, "y": Math.cos(i)});
+//         }
+//         return data;
+//     }
+// };
+//
+// // Set the prototype to be the person object (dynamic inheritance)
+// // All new ColoredPerson must inherit its properties.
+// grapher.prototype = {
+//     xMax : 10,
+//     xMin : -10,
+//     step : 0.1
+// };
+
+
+export {Grapher as default};
 
--- test/function-grapher.test.js
+++ test/function-grapher.test.js
@@ -1,18 +1,58 @@
 import {default as graphunc} from '../src/function-grapher';
 
 
-test('adds 1 + 2 to equal 3', () => {
-    expect(1 + 2).toBe(3);
+
+
+test('Test generated data on simple additive function', () => {
+
+    var func = (x) => x+1;
+    var graph = new graphunc(func);
+    graph.setStep(1);
+    graph.setXMin(-10);
+    graph.setXMax(10);
+
+    var expectedData = [ { x: -10, y: -9 },
+        { x: -9, y: -8 },
+        { x: -8, y: -7 },
+        { x: -7, y: -6 },
+        { x: -6, y: -5 },
+        { x: -5, y: -4 },
+        { x: -4, y: -3 },
+        { x: -3, y: -2 },
+        { x: -2, y: -1 },
+        { x: -1, y: 0 },
+        { x: 0, y: 1 },
+        { x: 1, y: 2 },
+        { x: 2, y: 3 },
+        { x: 3, y: 4 },
+        { x: 4, y: 5 },
+        { x: 5, y: 6 },
+        { x: 6, y: 7 },
+        { x: 7, y: 8 },
+        { x: 8, y: 9 },
+        { x: 9, y: 10 } ];
+    expect(graph.getData()).toEqual(expectedData);
+
+
 });
 
+test('xMax < xMin throw an error', () => {
 
+    var func = (x) => x+1;
 
+    var graph = new graphunc(func);
+    graph.setStep(1);
+    graph.setXMin(10);
+    graph.setXMax(-10);
 
-test('Generate x and y data', () => {
+    expect(
+        () => {
+            graph.getData();
+        }
+    ).toThrow()
 
-    var graph = new graphunc();
-    console.log(graph.getData());
 
 
 });
+
 
